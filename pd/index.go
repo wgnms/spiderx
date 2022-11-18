d5a1ba608f6d5aecf52d04cb936f68d79a26f85e
wgnms <48816203+wgnms@users.noreply.github.com>
2022-11-19 02:40:02 +0800

    Add files via upload

---
+++ pd/index.go
@@ -0,0 +1,17 @@
+package pd
+import (
+	"github.com/go-gota/gota/series"
+	"github.com/go-gota/gota/dataframe"
+	"fmt"
+)
+func Test2(){
+	series.New([]string{"z", "y", "d", "e"}, series.String, "col")
+	dataFrame := dataframe.New(
+		series.New([]string{"a", "b", "c", "d", "e"}, series.String, "alphas"),
+		series.New([]int{5, 4, 2, 3, 1}, series.Int, "numbers"),
+		series.New([]string{"a1", "b2", "c3", "d4", "e5"}, series.String, "alnums"),
+		series.New([]bool{true, false, true, true, false}, series.Bool, "state"),
+	)
+
+	fmt.Println(dataFrame)
+}
