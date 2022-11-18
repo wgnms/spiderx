ec34ebd7f066760f7bec65049f36bd57dec0d3db
wgnms <48816203+wgnms@users.noreply.github.com>
2022-11-19 02:00:38 +0800

    Add files via upload

---
+++ qt/index.go
@@ -0,0 +1,27 @@
+package qt
+
+import (
+	"github.com/therecipe/qt/core"
+	"github.com/therecipe/qt/widgets"
+	"os"
+)
+func Test(){
+	widgets.NewQApplication(len(os.Args), os.Args)
+
+	// left sider
+	splitterLeft := widgets.NewQSplitter2(core.Qt__Horizontal, nil)
+	textTop := widgets.NewQTextEdit2("左部文字", splitterLeft)
+	splitterLeft.AddWidget(textTop)
+
+	// right sider
+	splitterRight := widgets.NewQSplitter2(core.Qt__Vertical, splitterLeft)
+	textRight := widgets.NewQTextEdit2("右部文字", splitterRight)
+	textbuttom := widgets.NewQTextEdit2("下部文字", splitterLeft)
+	splitterRight.AddWidget(textRight)
+	splitterRight.AddWidget(textbuttom)
+
+	splitterLeft.SetWindowTitle("splitter")
+	splitterLeft.Show()
+
+	widgets.QApplication_Exec()
+}
