fc500e3fa1ab17c1ed4e21d48f8eefb953331b1b
wgnms <48816203+wgnms@users.noreply.github.com>
2022-11-19 02:22:20 +0800

    Update index.go

--- pandas/index.go
+++ pandas/index.go
@@ -1,10 +1,12 @@
 package pandas
 import (
+	"github.com/go-gota/gota"
 	"github.com/go-gota/gota/series"
 	"github.com/go-gota/gota/dataframe"
 	"fmt"
 )
 func Test() {
+	
 	series.New([]string{"z", "y", "d", "e"}, series.String, "col")
 	dataFrame := dataframe.New(
 		series.New([]string{"a", "b", "c", "d", "e"}, series.String, "alphas"),
@@ -13,4 +15,5 @@ func Test() {
 		series.New([]bool{true, false, true, true, false}, series.Bool, "state"),
 	)
 	fmt.Println(dataFrame)
+	fmt.Println(gota)
 }
